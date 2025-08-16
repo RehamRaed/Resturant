4059d53df10516e2ea7cdc127787f82383e4041b
RehamRaed <r409257797@gmail.com>
2025-08-16 18:10:30 +0300

    edits

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDy-JV7lz3ESEZGJ9yq0knzJgfn93M0xbU",
+  authDomain: "resturant-ba493.firebaseapp.com",
+  projectId: "resturant-ba493",
+  storageBucket: "resturant-ba493.firebasestorage.app",
+  messagingSenderId: "814540937660",
+  appId: "1:814540937660:web:77bc455899973b662a73b9",
+  measurementId: "G-2R4LH9Y6LL"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
